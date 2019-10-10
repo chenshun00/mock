ea7027364845cff958519db64a2227274448ddbe
chenshun <chenshun00@gmail.com>
2019-10-10 20:06:44 +0800

    Update TypeReference.java

--- src/main/java/com/mock/TypeReference.java
+++ src/main/java/com/mock/TypeReference.java
@@ -6,22 +6,28 @@ import java.lang.reflect.Type;
 /**
  * 类型引用
  * 用以获取泛型的类型
+ *
  * @param <T> 泛型
  */
-public abstract class TypeReference<T> {
+public class TypeReference<T> {
 
-  private final Type type;
+    private Type type;
 
-  public TypeReference() {
-    Type superClass = getClass().getGenericSuperclass();
-    if (superClass instanceof Class) {
-      throw new MockException("不支持的类型或者检查参数是否已经添加{},eg: Mock.mock(new TypeReference<Integer>(){})");
+    public TypeReference() {
+        Type superClass = getClass().getGenericSuperclass();
+        if (superClass instanceof Class) {
+            //throw new MockException("不支持的类型或者检查参数是否已经添加{},eg: Mock.mock(new TypeReference<Integer>(){})");
+            return;
+        }
+        type = ((ParameterizedType) superClass).getActualTypeArguments()[0];
     }
-    type = ((ParameterizedType) superClass).getActualTypeArguments()[0];
-  }
 
-  public Type getType() {
-    return type;
-  }
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
 
 }
